da8b7cbb230e0c98877f3ed3dac2c103bd8af78e
Roni <roniarnandaa@gmail.com>
2025-07-02 14:00:20 +0700

    Penyesuaian padding & justify

--- frontend/src/pages/home/Banner.jsx
+++ frontend/src/pages/home/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import bannerImg from '../../assets/banner.png';
 const Banner = () => {
   return (
-    <div className="flex flex-col md:flex-row-reverse py-12 justify-between items-center gap-12">
+    <div className="flex flex-col md:flex-row-reverse py-8 justify-between items-center gap-12">
       <div className="md:w-1/2 w-full flex items-center md:justify-end">
         <img src={bannerImg} alt="Banner" />
       </div>
@@ -11,7 +11,7 @@ const Banner = () => {
         <h1 className="md:text-5x text-2xl font-medium mb-7">
           New Realease This Week
         </h1>
-        <p className="mb-10">
+        <p className="mb-10 text-justify">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quis
           natus et iure eos iusto. Numquam adipisci aliquid minima nesciunt
           voluptatum? Quia vero corporis earum id veniam neque eveniet
