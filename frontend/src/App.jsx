8e9e5106455f684423f18d21c96e9441126468b4
Roni <roniarnandaa@gmail.com>
2025-07-02 15:28:19 +0700

    Footer Section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 import './index.css';
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <main className="min-h-screen max-w-screen-2xl mx-auto px-8 py-2 font-primary">
         <Outlet />
       </main>
-      <footer>Footer</footer>
+      <Footer />
     </>
   );
 }
